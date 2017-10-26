8b3e34048888b771b769a1d400492a04b40229a2
JimmyG <jimmy.grunert@gmail.com>
2017-10-26 17:11:08 +0200

    done modul 3 added props and references and so on...

--- FriendOrganizer2.Model/Friend.cs
+++ FriendOrganizer2.Model/Friend.cs
@@ -8,5 +8,10 @@ namespace FriendOrganizer2.Model
 {
     public class Friend 
     {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
